ec9b168f525948829a51f2c08c55e18808c97b62
MitchellWeg <mitchell.w@live.nl>
2020-07-24 14:18:24 +0200

    add test for commiting a transaction

--- tests/test_case.R
+++ tests/test_case.R
@@ -25,12 +25,24 @@ test_that("we can manipulate tables in the database", {
     expect_error(grepl(output, "mtcars"))
 })
 
-test_that("we can rollback a transaction", {
+test_that("we can commit a transaction", {
 
     dbBegin(conn)
 
     # we need to disable transaction here,
     # because it doesn't support nested transactions
+    dbWriteTable(conn, "mtcars", mtcars[1:5,], transaction=F, overwrite=T)
+    dbCommit(conn)
+
+    output <- dbListTables(conn)
+    expect_equal(grepl(output, "mtcars"), T)
+    dbRemoveTable(conn, "mtcars")
+})
+
+test_that("we can rollback a transaction", {
+
+    dbBegin(conn)
+
     dbWriteTable(conn, "mtcars", mtcars[1:5,], transaction=F, overwrite=T)
     dbRollback(conn)
 
