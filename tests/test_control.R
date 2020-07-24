180e23130d3c8579aaebf60d9904f0f1fd3c7611
MitchellWeg <mitchell.w@live.nl>
2020-07-24 13:41:01 +0200

    clean test up beforehand

--- tests/test_control.R
+++ tests/test_control.R
@@ -4,7 +4,10 @@ library(DBI)
 test_that("we can start and stop a monetdbserver from R", {
 
     datadir <- "/tmp/unit-test_database"
-    
+
+    # clean dir beforehand
+    unlink(datadir, T)
+
     # check if we can create a creation script
     script <- MonetDB.R::monetdb.server.setup(database.directory=datadir, dbname="db1", dbport=50001)
     expect_equal(script, '/tmp/unit-test_database/db1.sh')
