3e6a41adf2d974b55b36fd1b0c9a186871e5e7db
MitchellWeg <mitchell.w@live.nl>
2020-08-06 16:45:00 +0200

    use better function in tests

--- tests/test_dbWriteTable.R
+++ tests/test_dbWriteTable.R
@@ -2,21 +2,16 @@ library(DBI)
 library(testthat)
 conn <- dbConnect(MonetDB.R::MonetDB())
 
-# Checks if we can create a table,
-# list the tables and drop tables.
-test_that("we can manipulate tables in the database", {
+test_that("we can write a table to the database", {
 
     # assert we can write a table to the database.
     dbWriteTable(conn, "mtcars", mtcars[1:5,], overwrite=T)
-    output <- dbListTables(conn) # test dbList aswell
-    expect_equal(grepl(output, "mtcars"), T)
+    expect_equal(dbExistsTable(conn, "mtcars"), T)
+})
+
+test_that("we can drop a table", {
 
     # assert we can drop a table
     dbRemoveTable(conn, "mtcars")
-    output <- dbListTables(conn) 
-    expect_error(grepl(output, "mtcars"))
-})
-
-test_that("dbListFields returns the right amount of fields", {
-
+    expect_equal(dbExistsTable(conn, "mtcars"), F)
 })
