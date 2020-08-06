63882cbf562976ee672caac43b0a32e059908cfd
MitchellWeg <mitchell.w@live.nl>
2020-08-06 17:01:13 +0200

    add tests for dbSendUpdate

--- tests/test_dbWriteTable.R
+++ tests/test_dbWriteTable.R
@@ -1,5 +1,6 @@
 library(DBI)
 library(testthat)
+
 conn <- dbConnect(MonetDB.R::MonetDB())
 
 test_that("we can write a table to the database", {
@@ -9,9 +10,21 @@ test_that("we can write a table to the database", {
     expect_equal(dbExistsTable(conn, "mtcars"), T)
 })
 
+test_that("we can update a table", {
+    MonetDB.R::dbSendUpdate(conn, "CREATE TABLE foo(a INT, b INT)")
+    expect_equal(dbExistsTable(conn, "foo"), T)
+})
+
+test_that("we can update a table async", {
+    MonetDB.R::dbSendUpdate(conn, "CREATE TABLE foo1(a INT, b INT)")
+    expect_equal(dbExistsTable(conn, "foo"), T)
+})
+
 test_that("we can drop a table", {
 
     # assert we can drop a table
     dbRemoveTable(conn, "mtcars")
     expect_equal(dbExistsTable(conn, "mtcars"), F)
+    dbRemoveTable(conn, "foo")
+    dbRemoveTable(conn, "foo1")
 })
