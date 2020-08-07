3fce737b7b9a7bdcd536d6bef751159c7ce426fe
MitchellWeg <mitchell.w@live.nl>
2020-08-07 11:54:16 +0200

    add test for special unicode chars

--- tests/test_dbWriteTable.R
+++ tests/test_dbWriteTable.R
@@ -5,10 +5,22 @@ conn <- dbConnect(MonetDB.R::MonetDB())
 
 test_that("we can write a table to the database", {
 
-    # assert we can write a table to the database.
     dbWriteTable(conn, "mtcars", mtcars[1:5,], overwrite=T)
     expect_equal(dbExistsTable(conn, "mtcars"), T)
 })
+
+test_that("special characters work", {
+    table_name <- "specialcharsfoo"
+    pi <- enc2utf8('U+03C0')
+
+    dbExecute(conn, paste0("CREATE TABLE ", table_name, "(x TEXT);"))
+    dbExecute(conn, paste0("INSERT INTO ", table_name, " VALUES ('U+03C0'); "))
+
+    expect_equal(dbGetQuery(conn, "SELECT * FROM specialcharsfoo")$x, pi)
+
+    dbRemoveTable(conn, "specialcharsfoo")
+})
+
 
 test_that("we can update a table", {
     MonetDB.R::dbSendUpdate(conn, "CREATE TABLE foo(a INT, b INT)")
@@ -31,7 +43,7 @@ test_that("we can drop a table", {
 })
 
 
-test_that("we can create a temporary table", {
+test_that("we can create a temporary table, and that its actually temporary", {
     table_name <- "fooTempTable"
     dbCreateTable(conn, table_name, iris, temporary=T)
     expect_equal(dbExistsTable(conn, table_name), T)
