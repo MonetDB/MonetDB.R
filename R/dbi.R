6e1e9af999889a7cd8c7f18446518c87f57927a8
MitchellWeg <mitchell.w@live.nl>
2020-08-31 16:31:33 +0200

    use query for keywords instead of hardcoded values

--- R/dbi.R
+++ R/dbi.R
@@ -37,7 +37,7 @@ setMethod("dbConnect", "MonetDBDriver", def=function(drv, dbname="demo", user="m
     slashsplit <- strsplit(rest, "/", fixed=TRUE)
     hostport <- slashsplit[[1]][1]
     dbname <- slashsplit[[1]][2]
-    
+   
     # count the number of : in the string
     ndc <- nchar(hostport) - nchar(gsub(":","",hostport,fixed=T))
     if (ndc == 0) {
@@ -102,14 +102,18 @@ setMethod("dbConnect", "MonetDBDriver", def=function(drv, dbname="demo", user="m
   .mapiAuthenticate(connenv$socket, dbname, user, password, language=language)
   
   conn <- new("MonetDBConnection", connenv=connenv)
+
+  # Fill the MonetDB keywords
+  keywords <- dbGetQuery(conn, "SELECT * FROM sys.keywords")
+  reserved_monetdb_keywords <<- sort(unique(array(c(unlist(keywords)))))
+
   if (getOption("monetdb.sequential", F)) {
     message("MonetDB: Switching to single-threaded query execution.")
     dbSendQuery(conn, "set optimizer='sequential_pipe'")
   }
   attr(conn, "dbPreExists") <- TRUE
   conn
-}, 
-valueClass="MonetDBConnection")
+}, valueClass="MonetDBConnection")
 
 
 ### MonetDBConnection
@@ -276,44 +280,6 @@ setMethod("dbSendQuery", signature(conn="MonetDBConnection", statement="characte
 
   invisible(new("MonetDBResult", env=env))
   })
-
-# found from sql_scan.c and trial/error
-reserved_monetdb_keywords <- sort(unique(toupper(c(.SQL92Keywords, 
-"ADD", "ADMIN", "AFTER", "AGGREGATE", "ALL", "ALTER", "ALWAYS", 
-"AND", "ANY", "ASC", "ASYMMETRIC", "ATOMIC", "AUTO_INCREMENT", 
-"BEFORE", "BEGIN", "BEST", "BETWEEN", "BIGINT", "BIGSERIAL", 
-"BINARY", "BLOB", "BY", "CALL", "CASCADE", "CASE", "CAST", "CHAIN", 
-"CHAR", "CHARACTER", "CHECK", "CLOB", "COALESCE", "COMMIT", "COMMITTED", 
-"CONSTRAINT", "CONVERT", "COPY", "CORRESPONDING", "CREATE", "CROSS", 
-"CURRENT", "CURRENT_DATE", "CURRENT_ROLE", "CURRENT_TIME", "CURRENT_TIMESTAMP", 
-"CURRENT_USER", "DAY", "DEC", "DECIMAL", "DECLARE", "DEFAULT", 
-"DELETE", "DELIMITERS", "DESC", "DO", "DOUBLE", "DROP", "EACH", 
-"EFFORT", "ELSE", "ELSEIF", "ENCRYPTED", "END", "ESCAPE", "EVERY", 
-"EXCEPT", "EXCLUDE", "EXISTS", "EXTERNAL", "EXTRACT", "FALSE", 
-"FLOAT", "FOLLOWING", "FOR", "FOREIGN", "FROM", "FULL", "FUNCTION", 
-"GENERATED", "GLOBAL", "GRANT", "GROUP", "HAVING", "HOUR", "HUGEINT", 
-"IDENTITY", "IF", "ILIKE", "IN", "INDEX", "INNER", "INSERT", 
-"INT", "INTEGER", "INTERSECT", "INTO", "IS", "ISOLATION", "JOIN", 
-"LEFT", "LIKE", "LIMIT", "LOCAL", "LOCALTIME", "LOCALTIMESTAMP", 
-"LOCKED", "MEDIUMINT", "MERGE", "MINUTE", "MONTH", "NATURAL", 
-"NEW", "NEXT", "NOCYCLE", "NOMAXVALUE", "NOMINVALUE", "NOT", 
-"NOW", "NULL", "NULLIF", "NUMERIC", "OF", "OFFSET", "OLD", "ON", 
-"ONLY", "OPTION", "OR", "ORDER", "OTHERS", "OUTER", "OVER", "PARTIAL", 
-"PARTITION", "POSITION", "PRECEDING", "PRESERVE", "PRIMARY", 
-"PRIVILEGES", "PROCEDURE", "PUBLIC", "RANGE", "READ", "REAL", 
-"RECORDS", "REFERENCES", "REFERENCING", "REMOTE", "RENAME", "REPEATABLE", 
-"REPLICA", "RESTART", "RESTRICT", "RETURN", "RETURNS", "REVOKE", 
-"RIGHT", "ROLLBACK", "ROWS", "SAMPLE", "SAVEPOINT", "SECOND", 
-"SELECT", "SEQUENCE", "SERIAL", "SERIALIZABLE", "SESSION_USER", 
-"SET", "SIMPLE", "SMALLINT", "SOME", "SPLIT_PART", "STDIN", "STDOUT", 
-"STORAGE", "STREAM", "STRING", "SUBSTRING", "SYMMETRIC", "THEN", 
-"TIES", "TINYINT", "TO", "TRANSACTION", "TRIGGER", "TRUE", "UNBOUNDED", 
-"UNCOMMITTED", "UNENCRYPTED", "UNION", "UNIQUE", "UPDATE", "USER", 
-"USING", "VALUES", "VARCHAR", "VARYING", "VIEW", "WHEN", "WHERE", 
-"WHILE", "WITH", "WORK", "WRITE", "XMLAGG", "XMLATTRIBUTES", 
-"XMLCOMMENT", "XMLCONCAT", "XMLDOCUMENT", "XMLELEMENT", "XMLFOREST", 
-"XMLNAMESPACES", "XMLPARSE", "XMLPI", "XMLQUERY", "XMLSCHEMA", 
-"XMLTEXT", "XMLVALIDATE", "YEAR"))))
 
 # quoting
 quoteIfNeeded <- function(conn, x, warn=T, ...) {
