d688a611bfc786e78648bf167dee0c0d79baae57
Ying (Jennie) Zhang <ying.zhang@monetdbsolutions.com>
2021-02-26 14:19:40 +0100

    Added a test for the functions not already tested else where (dbGetRowCount and dbGetStatement)

--- tests/testthat/test-MonetDBResult.R
+++ tests/testthat/test-MonetDBResult.R
@@ -1,3 +1,19 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("we can query some metadata of a MonetDBResult object", {
+  conn <- dbConnect(MonetDB.R::MonetDB())
+  on.exit(dbDisconnect(conn))
+
+  tryCatch(
+    {
+      expect_equal(
+        dbWriteTable(conn, "usarrests", datasets::USArrests, temporary = TRUE), T
+      )
+      qry <- "SELECT * FROM usarrests"
+      res <- dbSendQuery(conn, qry)
+      expect_equal(dbGetRowCount(res), nrow(datasets::USArrests))
+      expect_equal(dbGetStatement(res), qry)
+    },
+    finally = {
+      expect_equal(dbRemoveTable(conn, "usarrests"), T)
+    }
+  )
 })
