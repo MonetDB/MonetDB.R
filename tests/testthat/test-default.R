f1ac1f9a8e532caeb91c22392b0fe1063da6ddb5
Ying (Jennie) Zhang <ying.zhang@monetdbsolutions.com>
2021-02-26 14:18:48 +0100

    unify test: always use on.exit(dbDisconnect(conn))

--- tests/testthat/test-default.R
+++ tests/testthat/test-default.R
@@ -3,7 +3,8 @@ library(DBI)
 test_that("we can find and connect to the default MonetDB database", {
   expect_equal(foundDefaultMonetDBdatabase(), T)
   conn <- monetdbDefault()
+  on.exit(dbDisconnect(conn))
+
   expect_equal(dbIsValid(conn), TRUE)
   expect_that(conn, is_a("MonetDBConnection"))
-  dbDisconnect(conn)
 })
