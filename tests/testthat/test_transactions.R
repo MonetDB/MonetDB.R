55792c6c9ab893c7e455f9545056427360299579
Ying (Jennie) Zhang <ying.zhang@monetdbsolutions.com>
2023-06-12 10:20:47 +0200

    Added an ACID test

--- tests/testthat/test_transactions.R
+++ tests/testthat/test_transactions.R
@@ -30,3 +30,29 @@ test_that("we can rollback a transaction", {
 
   expect_equal(dbExistsTable(conn, "mtcars"), F)
 })
+
+tsize <- function(conn, tname) {
+  as.integer(DBI::dbGetQuery(conn, paste0("SELECT COUNT(*) FROM ", tname))[[1]])
+}
+
+test_that("transactions are ACID", {
+  conn <- dbConnect(MonetDB.R::MonetDB())
+  on.exit(dbDisconnect(conn))
+
+  tname <- "monetdbtest"
+  dbSendQuery(conn, paste("create table", tname, "(a integer)"))
+  on.exit(dbRemoveTable(conn, tname), add = TRUE, after = FALSE)
+  expect_true(dbExistsTable(conn, tname))
+
+  dbBegin(conn)
+  dbSendQuery(conn, paste("INSERT INTO", tname, "VALUES (42)"))
+  expect_equal(tsize(conn, tname), 1)
+  dbRollback(conn)
+  expect_equal(tsize(conn, tname), 0)
+
+  dbBegin(conn)
+  dbSendQuery(conn, paste("INSERT INTO", tname, "VALUES (42)"))
+  expect_equal(tsize(conn, tname), 1)
+  dbCommit(conn)
+  expect_equal(tsize(conn, tname), 1)
+})
